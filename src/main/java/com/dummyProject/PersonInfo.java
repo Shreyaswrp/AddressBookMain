6ebe1d1f9c2aee9432deae12f38921b487902139
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-15 09:01:47 +0530

    Added:to create person's contact in address book

---
+++ src/main/java/com/dummyProject/PersonInfo.java
@@ -0,0 +1,15 @@
+package com.dummyProject;
+
+public class PersonInfo {
+
+    String firstName;
+    String lastName;
+    String city;
+    String state;
+    int zip;
+    long phoneNumber;
+
+    public String toString(){
+        return "firstName"+firstName+"lastName"+lastName+"city"+city+"state"+state+"zip"+zip+"phoneNumber"+phoneNumber;
+    }
+}
